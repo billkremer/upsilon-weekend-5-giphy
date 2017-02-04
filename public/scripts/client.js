0f250ebcdec34b97f1c78e6b2e7c80c984b7c3d3
Bill Kremer <bkrems12@gmail.com>
2017-02-04 17:55:11 -0600

    added references to the service in the favorites controller.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,5 @@
+var verbose = false; // lets messages be turned off for non-error console.logs
+
 var app = angular.module('goodGiphyApp', ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider) {
@@ -12,10 +14,36 @@ app.config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 }); // closes app.config
 
-app.controller('DefaultController', function () {
-  console.log('DefaultController is loaded!');
-});
+app.controller('DefaultController', function (GiphyService) {
+  if (verbose) console.log('DefaultController is loaded!');
+}); // closes DefaultController
 
-app.controller('FavoritesController', function () {
-  console.log('FavoritesController is loaded...');
-})
+app.controller('FavoritesController', ['GiphyService', function (GiphyService) {
+  if (verbose) console.log('FavoritesController is loaded...');
+
+  var ctrl = this;
+
+  var giphyList = []; // array of favorites objects
+
+// TODO // get the list of favorites?
+
+  ctrl.getFavoriteGiphys = function () {
+  if (verbose) console.log('inside client.js getFavs');
+  GiphyService.getFavoriteGiphys().then(function (res) {
+if (verbose)   console.log('inside client.js favGif res: ', res); // should be all favorite gifs?
+    // do something else? a message to the dom?
+  }); // close then.
+}; // closes getFavGif
+  //
+  //
+  // ctrl.removeGif = function (gifToRemove) {
+  // if (verbose)   console.log('inside client.js removeGif', gifToRemove);
+  //   GiphyService.removeGif(gifToRemove).then(function (res) {
+  // if (verbose)   console.log('inside client.js removeGif res: ', res);
+  //     // do something else? a message to the dom?
+  //   }); // close then.
+  // }; // closes removeGif
+
+
+
+}]); // closes FavoritesController
