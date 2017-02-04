243860f2d2589c2cb805edc2112aff0f08168fc7
Bill Kremer <bkrems12@gmail.com>
2017-02-03 22:54:35 -0600

    initial server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var express =  require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+// var todos = require('./routes/todos');
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// app.use('/todos', todos); // pass through to the todos.js route
+
+
+// serve the index page at /
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
+
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+  console.log('Listening on port ', server.address().port);
+  console.log('Press Control + C to exit');
+});
