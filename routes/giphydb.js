bb7cbf9fb709ff1428512a32ca730780f33df4bd
Bill Kremer <bkrems12@gmail.com>
2017-02-05 01:44:06 -0600

    added a route for dealing with the database."

--- routes/giphydb.js
+++ routes/giphydb.js
@@ -1,4 +1,4 @@
-var verbose = false; // lets messages be turned off for non-error console.logs
+var verbose = true; // lets messages be turned off for non-error console.logs
 
 var express = require("express");
 var router = express.Router();
@@ -12,10 +12,36 @@ var config = { database: "upsilon_giphy_favs" };
 // think of as 'how I connect to DB'
 var pool = new pg.Pool(config);
 
+var bodyParser = require("body-parser");
+router.use(bodyParser.json());
+// router.use(bodyParser.urlencoded({extended: true}));
+
+router.put('/:id', function(req, res) {  // updating the favorited Gifs
+console.log('what09871234');
+  console.log('giphydb PuT!', req.body, 'params', req.params);
+
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to database", err);
+      res.sendStatus(500);
+      done(); // returns the connection
+    } else {
+      client.query( 'UPDATE giphy_favs SET giphy_comment=$2  WHERE id = $1 RETURNING *', [req.body.id, req.body.giphy_comment ], function(err, result) {
+        done();
+        if (err) {
+          console.log("Error querying DB", err);
+          res.sendStatus(500);
+        } else {
+          if (verbose) console.log("Got put info from DB", result.rows);
+          res.send(result.rows);
+        };
+      });  // closes client query
+    }; // closes initial else
+  }); // closes pool.connection
+}); // closes POST
 
 
-
-router.get("/", function(req, res) {  // getting the favorited Gifs
+router.get("/getfavs", function(req, res) {  // getting the favorited Gifs
 
   console.log('giphydb get!');
   pool.connect(function(err, client, done) {
@@ -24,13 +50,13 @@ router.get("/", function(req, res) {  // getting the favorited Gifs
       res.sendStatus(500);
       done(); // returns the connection
     } else {
-      client.query( "SELECT * FROM giphy_favs;", function(err, result) {
+      client.query( "SELECT * FROM giphy_favs ORDER BY id ASC;", function(err, result) {
         done();
         if (err) {
           console.log("Error querying DB", err);
           res.sendStatus(500);
         } else {
-          if (verbose) console.log("Got info from DB", result.rows);
+          if (verbose) console.log("Got get info from DB", result.rows);
           res.send(result.rows);
         };
       });  // closes client query
