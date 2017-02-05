a7787a5ebf50e458912f13643152c27164e00f82
Bill Kremer <bkrems12@gmail.com>
2017-02-05 01:41:51 -0600

    added routes to the API if needed
    also cleaned up some syntax.

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var giphydb = require('./routes/giphydb');
+// var giphyAPI = require('./routes/giphyAPI');
+
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
- app.use('/favgifs', giphydb); // pass through to the todos.js route
+app.use('/favgifs', giphydb); // pass through to the giphydb.js route
+
+// app.use(('/getGifs', giphyAPI)); // pass through to the giphyAPI
 
 
-// send all requests to index.html
-app.get('/', function (req, res) {
+// send all other requests to index.html
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 
