da4d841bf4eb0a3ac91087f9a3244d928b462472
Bill Kremer <bkrems12@gmail.com>
2017-02-05 23:34:47 -0600

    added delete function to js and cleaned up
    added delete function so user can remove favorites from their list.
    
    also cleaned up spacing and shortened minor variable names.

--- public/scripts/giphy-fav-service.js
+++ public/scripts/giphy-fav-service.js
@@ -46,12 +46,11 @@ if (verbose) console.log('any response?',response);
 
 
 
-
   this.getFavoriteGiphys = function () {
 
     // return the promise to the caller
     return $http({
-      url: '/favgifs/getfavs',
+      url: '/favgifs',
       type: 'GET'
     }).then(function(response) {
       //   ctrl.pokemonList = response.data.results;
@@ -83,62 +82,20 @@ if (verbose) console.log('any response?',response.data);
   }; // closes updateFavoriteComment
 
 
+  this.removeGif = function (giphyToRemove) {
+      console.log('got a bit further...delete id: ', giphyToRemove.id, giphyToRemove, '/favgifs/'+ giphyToRemove.id);
+      // var dataObjToSend = {temp: giphyToUpdate}
 
+      return $http.delete('/favgifs/'+ giphyToRemove.id)
+      .then(function(response) {
+ if (verbose) console.log('any response from delete?', response);
+    // var returnObj = {response: 'OK Yo'};
+   return response;
 
-  // router.get("/current", function(req, res) {  // getting the current list of todos
-  //   pool.connect(function(err, client, done) {
-  //     if (err) {
-  //       console.log("Error connecting to database", err);
-  //       res.sendStatus(500);
-  //       done(); // returns the connection
-  //     } else {
-  //       client.query( "SELECT * FROM giphy_favs WHERE task_completed IS NULL ORDER BY task_due ASC", function(err, result) {
-  //         done();
-  //         if (err) {
-  //           console.log("Error querying DB", err);
-  //           res.sendStatus(500);
-  //         } else {
-  //           if (verbose) console.log("Got info from DB", result.rows);
-  //           res.send(result.rows);
-  //         };
-  //       });  // closes client query
-  //     }; // closes initial else
-  //   }); // closes pool.connection
-  // }); // closes get
+      }).catch(function(err) {
+        console.log('error getting random data from API :', err);
+      });
 
-
-
-  this.removeGif = function () {
-    // return the promise to the caller
-    // return $http.delete()
-
-  } // closes removeGif
-
-
-
-
-
-
-
-  // this.getAllPokemon = function () {
-  // // return the promise to the caller
-  //   return $http.get(API + '/pokemon').then(function(response){
-  //     console.log('Got a response from the API', response);
-  //     return response.data.results; // returned on resolution of promise
-  //   }).catch(function(err){
-  //     console.log('Error getting info from API', err);
-  //   });
-  // }; // closes getAllPokemon
-  //
-  // this.getOnePokemon = function (pokemon) {
-  //   return $http.get(pokemon.url).then(function(response){
-  //     console.log('Pokemon info', response.data);
-  //     var foundPokemon = response.data;
-  //     return foundPokemon.sprites.front_default; // should be an image
-  //   }).catch(function(err){
-  //     console.log('Error getting info from API', err);
-  //   });
-  // }; // closes getOnePokemon
-
+  }; // closes updateFavoriteComment
 
 });// sevice closer
